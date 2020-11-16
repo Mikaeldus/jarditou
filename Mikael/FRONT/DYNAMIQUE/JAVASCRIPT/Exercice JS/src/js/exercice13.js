296963f70f4a0ffcddf960e7e252e13ea41ece56
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-16 08:48:35 +0100

    Mise à jour Suivi

--- Mikael/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice JS/src/js/exercice13.js
+++ Mikael/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice JS/src/js/exercice13.js
@@ -1,43 +1,43 @@
 window.onload = (() => {
 
-// On Associe le click des buttons à l'appel des fonctions
-document.getElementById('btnExo1').addEventListener('click',clickExo1 );
+    // On Associe le click des buttons à l'appel des fonctions
+    document.getElementById('btnExo1').addEventListener('click', clickExo1);
 
 })
 
-function clickExo1(){
+function clickExo1() {
     // j'attribut à ma variable tableau une valeur
-    var tableau =[];
+    var tableau = [];
     // je lance mes functions si dans le tableau ce ne sont pas des nombres ou si c'est 0  il return else 
     tableau = saisieTab(tableau);
-        if(!isNaN(tableau)){
-            infoTab(tableau);
-        }
-        else{
-            alert(`Veuillez recommencer !! `);
-        }
+    if (!isNaN(tableau)) {
+        infoTab(tableau);
+    }
+    else {
+        alert(`Veuillez recommencer !! `);
+    }
 }
 
 
-function saisieTab(){
+function saisieTab() {
     var data = prompt(`Veuillez saisir vos données, séparer par un ";"`);
-            tableau = data.split(";");
-            return tableau;   
+    tableau = data.split(";");
+    return tableau;
 }
 
-function infoTab(tableau){
+function infoTab(tableau) {
     // Et si le tableau contient des lettres ?
     var notNumberArray = false;
 
     tableau.forEach((value) => {
         var valueParsed = parseInt(value);
 
-        if(!valueParsed) {
+        if (!valueParsed) {
             notNumberArray = true;
         }
     })
 
-    if(!notNumberArray) {
+    if (!notNumberArray) {
         var max = Math.max(...tableau);
         var somme = 0;
 
@@ -48,6 +48,6 @@ function infoTab(tableau){
         var moyenne = somme / tableau.length;
         alert(`La moyenne : ${moyenne}`);
         alert(`Le max : ${max}`);
-        alert(`Voici le Tableau ${tableau}`); 
+        alert(`Voici le Tableau ${tableau}`);
     }
 }
