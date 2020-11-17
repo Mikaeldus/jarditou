bfed367215ff385bb3d43c0791daff9c65bb83f8
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-17 08:52:37 +0100

    Mise à jour Suivi et commentaire JS

--- Mikael/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice JS/src/js/exercice15.js
+++ Mikael/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice JS/src/js/exercice15.js
@@ -2,59 +2,82 @@ window.onload = (() => {
 
     // On Associe le click des buttons à l'appel des fonctions
     document.getElementById('button1').addEventListener('click', clickExo1);
-    document.getElementById('button2').addEventListener('click', clickExo2);
+    document.getElementById('button2').addEventListener('click', verif);
     document.getElementById('button3').addEventListener('click', generer);
 })
 
 
-// ----------- Ecercice 1 ---------
-clickExo1 = (() =>{
 
+
+
+
+
+// ----------- Ecercice 1 ---------
+// je creer la function 
+clickExo1 = (() => {
+    // je recupere ma valeur dans ma page html et je creer ma variable  
     var text = document.getElementById('text').value;
-    if(isNaN(text)){
+    // Si ma variable est des nombres alors le resultat est else 
+    if (isNaN(text)) {
         alert(`Vous avez saisie ${text}`);
     }
-    else{
+    else {
         alert(`Veuillez saisir un champ valide !`);
     }
-     
+
 })
 // ---------------Exo 1 -----------------
+
+
+
+
+
+
+
 // -----------------Exo 2 -----------------
+// je creer mes variables pour les initialisées
 var chiffre = 0;
 var i = 1;
-
-
-
 var vide = document.getElementById('aleatoire');
 
 
-
-
-
-generer = (() =>{
+// je creer ma function pour généré un nombre aléatoire compris entre 0-20
+generer = (() => {
     aleatoire.textContent = '';
     i = 1;
     chiffre = parseInt(Math.random() * 20);
+    // controle sur console 
     console.log(chiffre);
+    // je retourne ma valeur chiffre 
     return chiffre;
 })
 
-clickExo2 = (() =>{
+// je creer ma function vérif 
+verif = (() => {
+    // je declare ma variable qui utilise la saisie part l'utilisateur 
+    var nombre = document.getElementById(`usernombre`).value;
 
-        var nombre = document.getElementById (`usernombre`).value;
-        console.log(nombre);
-        if (isNaN(nombre)) {
-            aleatoire.textContent = `Veuillez saisir un nombre entre 1 et 20 merci`;
-        }else{
-   
-        if(parseInt(nombre) == chiffre ){
+    console.log(nombre);
+    // controle console 
+
+    // Si la valeur de nombre n'est pas des nombres cela return NAN 
+    if (isNaN(nombre)) {
+        // je remplit la champ vide part une demande pour l'utilisateur 
+        aleatoire.textContent = `Veuillez saisir un nombre entre 1 et 20 merci`;
+    } else {
+        // Si nombre est égale à chiffre :
+        if (parseInt(nombre) == chiffre) {
             aleatoire.textContent = `Félicitation, vous avez trouver le bon nombre ${chiffre} en ${i} coup(s) :)`;
             console.log(`Félicitation, vous avez trouver le bon nombre ${chiffre} en ${i} coup(s) :`);
-        }else if (parseInt(nombre) < chiffre){
+
+
+            // et si nombre est inférieur a chiffre 
+        } else if (parseInt(nombre) < chiffre) {
             aleatoire.textContent = `Plus grand`;
             console.log(`Plus grand`);
-        }else{
+
+            // Sinon si nombre est supérieur à chiffre : 
+        } else {
             aleatoire.textContent = `Plus petit`;
             console.log(`Plus petit`);
             i++;
