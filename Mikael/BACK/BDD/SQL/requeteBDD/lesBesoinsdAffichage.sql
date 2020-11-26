ac2c22d00aa3039968f8c9a8a9f592b463f4d268
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-26 15:34:44 +0100

    Exo BDD

--- Mikael/BACK/BDD/SQL/requeteBDD/lesBesoinsdAffichage.sql
+++ Mikael/BACK/BDD/SQL/requeteBDD/lesBesoinsdAffichage.sql
@@ -13,10 +13,8 @@ GROUP BY fournis.numfou
  
 
 --3.Afficher le nombre de commandes fournisseurs passées, et le nombre de fournisseur concernés.
-SELECT fournis.numfou, COUNT(entcom.numcom)
-FROM entcom
-JOIN fournis ON entcom.numfou = fournis.numfou 
-GROUP BY fournis.numfou
+SELECT COUNT(numcom) AS 'Nombre de commande'  , COUNT(DISTINCT numfou) AS 'Fournisseur concerné' 
+FROM entcom 
 
 --4.Editer les produits ayant un stock inférieur ou égal au stock d'alerte et dont la quantité annuelle est inférieur est inférieure à1000 (informations à fournir : n° produit, libelléproduit, stock, stockactuel d'alerte, quantitéannuelle)
 SELECT codart, libart, stkale, stkphy, qteann
@@ -82,4 +80,69 @@ WHERE obscom LIKE '%urgent%'
 GROUP BY entcom.numcom, nomfou, libart
 ORDER BY soustotal DESC
 
---12 Coder de 2manières différentes la requête suivante:Lister lenom desfournisseurs susceptibles de livrer au moins un article
+--12 Coder de 2manières différentes la requête suivante:Lister le nom des fournisseurs susceptibles de livrer au moins un article
+SELECT nomfou
+FROM entcom
+JOIN fournis ON entcom.numfou = fournis.numfou
+JOIN ligcom ON entcom.numcom = ligcom.numcom AND qteliv >= 1
+GROUP BY nomfou
+
+--OU--
+
+SELECT nomfou
+FROM entcom, fournis, ligcom
+WHERE entcom.numfou = fournis.numfou and entcom.numcom = ligcom.numcom  AND qteliv >= 1
+GROUP BY nomfou
+
+--13 Coder de 2 manières différentes la requête suivante Lister les commandes (Numéro et date) dont le fournisseur est celui de la commande 70210
+SELECT numcom, datcom
+FROM entcom
+WHERE numfou = 120
+
+--OU--
+SELECT nomfou, numcom, datcom
+FROM fournis 
+JOIN entcom ON entcom.numfou = fournis.numfou
+WHERE fournis.nomfou LIKE 'GROBRIGAN'
+
+--14 Dans les articles susceptibles d’être vendus, lister les articles moins chers (basés sur Prix1) que le moins cher des rubans (article dont le premier caractère commence par R). On affichera le libellé de l’article et prix1
+SELECT libart, prix1
+FROM vente, produit
+WHERE produit.codart = vente.codart and STKPHY > 0 and PRIX1 < (
+SELECT min(prix1) 
+FROM VENTE, PRODUIT 
+WHERE produit.CODART = vente.CODART and LIBART  LIKE ('R%') )
+
+GROUP BY LIBART,PRIX1
+
+--15 Editer la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte. La liste est triée par produit puis fournisseur
+SELECT libart, fournis.nomfou
+FROM produit
+JOIN vente ON produit.codart = vente.codart
+JOIN fournis ON fournis.numfou = vente.numfou AND stkphy <=(
+SELECT SUM(stkale * 1.5)
+FROM produit
+WHERE stkale > 0 AND stkphy > 0)
+
+ORDER BY libart, fournis.nomfou
+
+--16 Éditer la liste des fournisseurs susceptibles de livrer les produit dont le stock est inférieur ou égal à 150 % du stock d'alerte et un délai de livraison d'au plus 30 jours. La liste est triée par fournisseur puis produit
+SELECT libart, fournis.nomfou
+FROM produit
+JOIN vente ON produit.codart = vente.codart
+JOIN fournis ON fournis.numfou = vente.numfou AND stkphy <=(
+SELECT SUM(stkale * 1.5)
+FROM produit
+WHERE stkale > 0 AND stkphy > 0 AND delliv < 30)
+
+ORDER BY libart, fournis.nomfou
+
+--17 Avec le même type de sélection que ci-dessus, sortir un total des stocks par fournisseur trié par total décroissant
+SELECT nomfou, sum(stkphy) AS stock
+FROM  fournis
+JOIN vente ON vente.numfou = fournis.numfou
+JOIN produit ON vente.codart = produit.codart
+GROUP BY nomfou
+ORDER BY Stock DESC
+
+--18 En fin d'année, sortir la liste des produits dont la quantité réellement commandée dépasse 90% de la quantité annuelle prévue.
