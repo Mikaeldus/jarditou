1d5f2d96c85dbe686beeca291ef311b507b29fc6
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-27 11:25:15 +0100

    Evaluation SQL Deuxieme EXO

--- Mikael/BACK/BDD/SQL/Evaluation/Exercice2/Evaluation.sql
+++ Mikael/BACK/BDD/SQL/Evaluation/Exercice2/Evaluation.sql
@@ -26,5 +26,27 @@ GROUP BY suppliers.CompanyName
 ORDER BY nbre_produits DESC 
 -- je demande le nom des fournisseur, ensuite je compte le nombre de produit vendu part fournisseur  et avec le where je demande que les fournisseur qui sont de la France.
 
+
 -- 4 Liste des clients Français ayant plus de 10 commandes :
+SELECT orders.ShipName AS Client, COUNT(orders.CustomerID) AS nbre_commandes
+FROM orders
+WHERE orders.ShipCountry = 'France'
+GROUP BY orders.ShipName
+HAVING COUNT(orders.CustomerID) > 10 
+-- Je demande le nom de client, je compte le nombre de commande part client, je trie avec un where pour que ce soit que les client qui viennent de France,
+-- si le nombre de commande est inférieur à 10 je ne l'ai montre pas (avec le HAVING)
+
+
+-- 5 Liste des clients ayant un chiffre d’affaires > 30.000 :
+SELECT orders.ShipName AS Client, SUM(unitPrice * Quantity) AS CA, orders.ShipCountry AS Pays
+FROM northwind.`order details`
+JOIN orders ON orders.OrderID = northwind.`order details`.OrderID
+GROUP BY orders.ShipName, orders.ShipCountry
+HAVING SUM(unitPrice * Quantity) > 30000
+ORDER BY CA DESC
+-- j'affiche le client, le chiffre d'affaire en prenant les info de la talbe order details et le pays, 
+-- le having me permet d'afficher que les client qui ont un chiffres d'affaire supérieur à 30000, et j'utilise DESC pour montrer en décroissant.
+
+
+--6 Liste des pays dont les clients ont passé commande de produits fournis par « Exotic Liquids » :
 
