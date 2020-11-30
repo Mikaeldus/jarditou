5ffb1076946ceae4b3d70441e475950b04da83e7
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-30 15:12:25 +0100

    Exo05 PHP

---
+++ Mikael/BACK/BDD/PHP/Base/05/exercice3.php
@@ -0,0 +1,72 @@
+<?php
+    $nombre1 = 0;
+    $nombre2 = 0;
+?>
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+    <meta charset="utf-8">
+    <title>Exercice php</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<style>
+	caption{
+		border-style:solid;
+	}
+	table{
+		text-align:center;
+		border-style:solid;
+	}
+	th{
+		border-style:solid;
+	}
+	td{
+		border-style:solid;
+	}
+	</style>
+</head>
+<body>
+	<div>
+    <table>
+        <caption><strong>Table de multiplication</strong></caption>
+		
+        <thead>
+            <tr>
+			<th></th>
+				<?php
+				for($nombre1 = 0; $nombre1 <= 12; $nombre1++){
+					?>
+				<th>
+					<?= $nombre1 ?>
+				</th>
+				<?php
+				}
+				?>
+            </tr>
+        </thead>
+		
+        <tbody>
+			<?php
+				for($nombre2 =0; $nombre2 <=12; $nombre2++){
+				?>
+            <tr>
+                <th>
+				<?= $nombre2 ?>
+				</th>
+				<?php
+				for($nombre1 = 0; $nombre1 <=12; $nombre1++){
+					?>
+                <td>
+				<?= $nombre2 * $nombre1 ?>
+				</td>
+				<?php
+				}
+				?>
+            </tr>
+			<?php
+				}
+				?>
+        </tbody>
+    </table>
+	</div>
+</body>
+</html>
