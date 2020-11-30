51bc3e03b68e3bc283a16b414ece8718ccca7e3e
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-11-30 15:57:59 +0100

    Exo07 PHP

--- Mikael/BACK/BDD/PHP/Base/07/formulaire.php
+++ Mikael/BACK/BDD/PHP/Base/07/formulaire.php
@@ -1,22 +1,37 @@
-<?php
-    $nombre1 = 0;
-    $nombre2 = 0;
-?>
 <!DOCTYPE html>
-<html lang=fr>
+<html lang="fr">
+<!-- ----------HEAD--------- -->
+
 <head>
-    <meta charset="utf-8">
-    <title>Exercice php</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>PAGE ACCUEIL</title>
+
+  <!-- Bootstrap/css -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+<!-- ------JS-------- -->
+<script src="./assets/js/script.js"></script>
+    
 </head>
+
+<!-- ---------HEAD---------- -->
+
+<!-- --------------Body------------- -->
+
 <body>
-	<div>
+  
+
+    <!-- --------------Formulaire------------- -->
+
+
     <div class="container-fluid">
       <div class="row no-gutter">
         <div class="col-12">
 
           
-          <form action="" method="POST">
+          <form action="verif.php" method="POST">
             <div class="form-row">
 
               <p class="col-12">* Ces zones sont obligatoire</p>
@@ -123,6 +138,24 @@
         </div>
       </div>
     </div>
-	</div>
+
+
+    <!-- --------------Formulaire------------- -->
+    <!-- Optional JavaScript -->
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"></script>
+  </div>
 </body>
+
+<!-- --------------Body------------- -->
+
 </html>
