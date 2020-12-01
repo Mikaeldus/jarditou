639565500bd1e1deeec6f7d27105684526210920
MikaelDus <Mikael01.dusaussoy@gmail.com>
2020-12-01 13:04:22 +0100

    Exo07 PHP

--- Mikael/BACK/BDD/PHP/Base/07/formulaire.php
+++ Mikael/BACK/BDD/PHP/Base/07/formulaire.php
@@ -10,9 +10,10 @@
   <!-- Bootstrap/css -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/style.css">
 <!-- ------JS-------- -->
-<script src="./assets/js/script.js"></script>
+
+
+
     
 </head>
 
@@ -21,123 +22,31 @@
 <!-- --------------Body------------- -->
 
 <body>
-  
+
 
     <!-- --------------Formulaire------------- -->
 
 
+	
+
     <div class="container-fluid">
       <div class="row no-gutter">
         <div class="col-12">
-
+		
           
-          <form action="verif.php" method="POST">
-            <div class="form-row">
-
-              <p class="col-12">* Ces zones sont obligatoire</p>
-
-              <div class="col-md-6 mb-3">
-                <label for="nom">Nom</label>
-                <input type="text" class="form-control" id="inputNom" name="Nom" value="" >
-                <span id="missNom" class="hint missNom"></span>
-              </div>
-
-              <div class="col-md-6 mb-3">
-                <label for="prenom">Prénom</label>
-                <input type="text" class="form-control" id="inputPrenom" name="Prénom" value="" >
-                <span id="missPrenom" class="hint missPrenom"></span>
-              </div>
-
-            </div>
-
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
-                <label for="date">Date de naissance:</label>
-                <input type="date" class="form-control" id="inputDate" name="Date-de-naissance" value="">
-              </div>
-
-              <div class="col-md-6 mb-3">
-                <label for="mail">E-mail</label>
-                <input type="email" class="form-control" id="inputEmail" name="E-mail" value="" >
-                <span id="missMail" class="hint missMail"></span>
-              </div>
-
-            </div>
-
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
-                <label for="adresse">Adresse</label>
-                <input type="text" class="form-control" id="inputAdresse" name="Adresse" value="" >
-              </div>
-
-              <div class="col-md-6 mb-3">
-                <p>Sexe*:</p>
-                <input type="radio" name="sexe" value="f" required="required">Masculin
-                <input type="radio" name="sexe" value="m" required="required">Féminin<br>
-              </div>
-            </div>
-
-            <div class="form-row">
-
-              <div class="col-md-6 mb-3">
-                <label for="validationDefault06">Ville</label>
-                <input type="text" class="form-control" id="validationDefault06" name="Ville" >
-              </div>
-
-              <div class="col-md-6 mb-3">
-                <label for="code">Code Postale</label>
-                <input type="number" class="form-control" id="inputCodePostal" name="Code-postale">
-                <span id="missCodePostale" class="hint missCodePostale"></span>
-              </div>
-
-            </div>
-
-            <div class="form-row">
-
-              <div class="col-md-6 mb-3">
-
-                <div>Votre demande</div>
-                <label for="Sujet">Sujet*</label>
-                <select name="Sujet" id="inputSujet" required>
-                  <option value="" selected>Veuillez séléctionner un sujet</option>
-                  <option value="Mes commandes">Mes commandes</option>
-                  <option value="Question sur un produit">Question sur un produit</option>
-                  <option value="Réclamation">Réclamation</option>
-                  <option value="Autres">Autres</option>
-                </select><br>
-                <div>Votre question* :</div>
-                <textarea name="Commentaires" rows="6" cols="40" id="inputQuestion" required="required"></textarea>
-              </div>
-            </div>
-            <br>
-
-
-
-            <div class="form-group">
-              <div class="form-check">
-                <div class="col-md-12 mb-6">
-                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" name="autoriser" required>
-                  <label class="form-check-label" for="invalidCheck2">
-                    J'accepte le traitement informatique de ce formulaire
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-12 mb-6">
-              <div class="form-group">
-                <div class="form-check">
-                  <button id="btn-verif-formulaire" class="btn btn-primary" type="submit">Envoyer</button>
-                  <input class="btn btn-primary" type="reset" value="annuler">
-                </div>
-              </div>
-            </div>
-          </form>
+		<form enctype="multipart/form-data" action="./function.php" method="post">
+			
+		  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+	  	Envoyez ce fichier : <input name="userfile" type="file" />
+			<input type="submit" value="Envoyer le fichier" />
+		</form>
 
 
         </div>
       </div>
     </div>
+
+
 
 
     <!-- --------------Formulaire------------- -->
